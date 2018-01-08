b6be5c82b69f4fe4442c32901e5277c0bd92194b
Dmitry Bashkatov <dbashkatov@gmail.com>
2018-01-08 21:07:33 +0300

    Launch: add countdown option

--- stages/Launch.py
+++ stages/Launch.py
@@ -4,6 +4,8 @@ from .BaseStage import BaseStage
 
 
 class Launch(BaseStage):
+    countdown = 3
+
     def execute(self):
         if self.vessel.situation.name not in ('pre_launch', 'landed'):
             self.log.error('Invalid situation: %s', self.vessel.situation)
@@ -11,13 +13,15 @@ class Launch(BaseStage):
 
         self.vessel.control.throttle = 1.0
 
-        # Countdown...
-        self.log.info('3...')
-        time.sleep(1)
-        self.log.info('2...')
-        time.sleep(1)
-        self.log.info('1...')
-        time.sleep(1)
-        self.log.info('Launch!')
+        if self.countdown < 1:
+            time.sleep(1)
+        else:
+            self.exec_countdown()
 
+        self.log.info('Launch!')
         self.vessel.control.activate_next_stage()
+
+    def exec_countdown(self):
+        for i in range(self.countdown, 0, -1):
+            self.log.info('%s...', i)
+            time.sleep(1)
