bd80b2edba5a1be3e7c6b9e025d8384fee15a593
Dmitry Bashkatov <dbashkatov@gmail.com>
2018-01-10 00:47:19 +0300

    separate NodeManoeuvre stage

--- stages/Circularize.py
+++ stages/Circularize.py
@@ -1,25 +1,14 @@
 import math
 
-import math2
-from .BaseStage import BaseStage
+from .NodeManoeuvre import NodeManoeuvre
 
 
-class Circularize(BaseStage):
-    lead_time = 5
-    max_delta_v_error = 0.1
-    burn_slowdown_factor = 8.37
-
+class Circularize(NodeManoeuvre):
     def execute(self):
-        self.log.info('Planning circularization burn')
+        self.log.info('Planning circularization manoeuvre')
         delta_v = self.calc_delta_v()
-        self.log.info('DeltaV: %s', delta_v)
         node = self.add_node(self.conn.space_center.ut + self.vessel.orbit.time_to_apoapsis, prograde=delta_v)
-        burn_time = self.calc_burn_time(delta_v)
-        self.log.info('Burn time: %s', burn_time)
-
-        self.orientate(node)
-        self.wait_until_burn(node, burn_time)
-        self.execute_burn(node, burn_time)
+        self.perform_manoeuvre(node)
 
     def calc_delta_v(self):
         # vis-viva equation
@@ -31,56 +20,3 @@ class Circularize(BaseStage):
         v1 = math.sqrt(mu * ((2. / r) - (1. / a1)))
         v2 = math.sqrt(mu * ((2. / r) - (1. / a2)))
         return v2 - v1
-
-    def calc_burn_time(self, delta_v):
-        # rocket equation
-
-        F = self.vessel.available_thrust
-        Isp = self.vessel.specific_impulse * 9.82
-        m0 = self.vessel.mass
-        m1 = m0 / math.exp(delta_v / Isp)
-        flow_rate = F / Isp
-        return (m0 - m1) / flow_rate
-
-    def orientate(self, node):
-        self.log.info('Orientating ship for circularization burn')
-        self.vessel.auto_pilot.engage()
-        self.vessel.auto_pilot.reference_frame = node.reference_frame
-        self.vessel.auto_pilot.target_direction = (0, 1, 0)
-        self.vessel.auto_pilot.wait()
-
-    def wait_until_burn(self, node, burn_time):
-        self.log.info('Waiting until circularization burn')
-        self.conn.space_center.warp_to(node.ut - self.lead_time - (burn_time / 2.))
-
-        self.log.info('Ready to execute burn')
-        with self.conn.stream(getattr, node, 'time_to') as time_to_node:
-            while time_to_node() - (burn_time / 2.) > 0:
-                pass
-
-    def execute_burn(self, node, burn_time):
-        self.log.info('Executing burn')
-
-        slowdown_dv = burn_time * self.burn_slowdown_factor
-
-        with self.conn.stream(getattr, node, 'remaining_delta_v') as remaining_delta_v:
-            dv = remaining_delta_v()
-            prev_dv = dv
-
-            while self.max_delta_v_error < dv:
-                if self.quantize_dv(dv) > self.quantize_dv(prev_dv):
-                    self.log.error('Breaking burn due to increasing manoeuvre delta-V')
-                    break
-
-                prev_dv = dv
-                dv = remaining_delta_v()
-
-                if dv > slowdown_dv:
-                    self.vessel.control.throttle = 1.0
-                else:
-                    self.vessel.control.throttle = dv / slowdown_dv
-
-        self.vessel.control.throttle = 0.0
-
-    def quantize_dv(self, dv):
-        return math2.quantize(dv, self.max_delta_v_error)
